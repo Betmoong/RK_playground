088cfc648aa320045aa50fcf1f5a01a3f35d99a0
Rocky <rdev99@naver.com>
2024-04-03 09:31:48 +0900

    Initial Setting

---
+++ TCA_playground/App/TCA_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_playgroundApp.swift
+//  TCA_playground
+//
+//  Created by Rocky on 4/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TCA_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
